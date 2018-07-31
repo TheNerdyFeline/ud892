d7ec945d6f525748fded6c629d16615287d1c604
Jackie Wellons <jmw5050@gmail.com>
2018-07-31 11:51:18 -0700

    gulp sass works

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -3,14 +3,15 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
-	console.log('hello world!');
+  console.log('hello world!');
 });
 
-gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('./css'));
+gulp.task('styles', function(done) {
+  gulp.src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./css'));
+  done();
 });
